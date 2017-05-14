6b49f6e99963558c225153da31a831bf780f02a5
yalewoo <yalewoo@icloud.com>
2017-05-14 15:10:46 +0800

    logging improvement

--- fetch.py
+++ fetch.py
@@ -1,28 +1,41 @@
 import time
 import urllib.request
+import sys
 def getpath():
 	conf=open('today.config','r')
 	st=conf.read()
 	key,pt=st.split('=')
 	if key=='path':
 		return pt
+
+def usestd(b):
+	if b==False:
+		lout=open('log','w+')
+		sys.stdout=lout
+
+def logger(*dt):
+	ts=time.asctime()
+	print('[',ts,']',dt)
 if __name__ == '__main__':
-	try:
-		flag=True
-		rem=urllib.request.urlopen('http://www.ipip5.com/today/api.php?type=txt')
-		if rem.getcode()!=200:
-			flag=False
-	except Exception as e:
-		flag=False
-		print('An error occured:',e)
-
-	if flag==True:
-		fout=open(getpath(),'wb+')
+	usestd(False)
+	while True:
 		try:
-			text=rem.read()
-			fout.write(text)
+			flag=True
+			rem=urllib.request.urlopen('http://www.ipip5.com/today/api.php?type=txt')
+			if rem.getcode()!=200:
+				flag=False
 		except Exception as e:
-			print('An error occured:',e)
-		fout.close()
-
-	time.sleep(1800)
+			logger('Error:',e)
+			flag=False
+				
+		if flag==True:
+			logger('TXT fetch success')
+			fout=open(getpath(),'wb+')
+			try:
+				text=rem.read()
+				fout.write(text)
+			except Exception as e:
+				logger('Error:',e)
+			fout.close()
+			logger('TXT update done')
+		time.sleep(1800)
